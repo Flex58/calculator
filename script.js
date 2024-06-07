337db5f050d50edc6acb8a24db8192789e9ad1af
Flex58 <164533598+Flex58@users.noreply.github.com>
2024-06-07 11:33:30 +0200

    clean css and index and add basic calculator func

--- script.js
+++ script.js
@@ -1,27 +1,18 @@
- operate()
+const operatorBtn = document.querySelectorAll(".operator");
+const numberBtn = document.querySelectorAll(".number");
+const display = document.querySelector("#display")
+const equals = document.querySelector("#equals")
 
-function operate(){
-    let num1 = prompt("Number: ");
-    let operator = prompt("Operator: ")
-    let num2 = prompt("Number: ");
-
+function operate(num1, operator, num2){
     switch (operator) {
-        case "+":
-            console.log(add(Number(num1), Number(num2)));
-            break;
-        case "-":
-            console.log(subtract(Number(num1), Number(num2)));
-            break;
-        case "*":
-            console.log(multiply(Number(num1), Number(num2)));
-            break;
-        case "/":
-            console.log(divide(Number(num1), Number(num2)));
-            break;
-        default:
-            alert("Enter a valid operator (+, -, *, /)")
-            operate()
-            break;
+        case "plus":
+            return add(Number(num1), Number(num2));
+        case "minus":
+            return subtract(Number(num1), Number(num2));
+        case "multiply":
+            return multiply(Number(num1), Number(num2));
+        case "divide":
+            return divide(Number(num1), Number(num2));
     }   
 }
 
@@ -44,3 +35,23 @@ function divide (x, y) {
     let result = x / y;
     return result
 } 
+let displayNum = [];
+numberBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        displayNum.push(btn.id)
+        display.textContent = displayNum.join("")
+    })
+ }) 
+let num;
+let operator;
+operatorBtn.forEach((btn) =>{
+    btn.addEventListener("click", () =>{
+        if (operator == undefined){
+            num = displayNum.join("")
+        }
+        operator = btn.id
+        displayNum = [];
+    })
+})
+
+equals.addEventListener("click", () => display.textContent = operate(num, operator, displayNum.join("")))
