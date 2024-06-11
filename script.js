1f864d4b955e03ae5e39e3c0c50a38721b28eaa5
Flex58 <164533598+Flex58@users.noreply.github.com>
2024-06-11 10:20:16 +0200

    enhance keyboard support

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ let num2;
 
 function operate(num1, operator, num2){
     switch (operator) {
-        case "plus":
+        case "+":
             return add(Number(num1), Number(num2));
-        case "minus":
+        case "-":
             return subtract(Number(num1), Number(num2));
-        case "multiply":
+        case "*":
             return multiply(Number(num1), Number(num2));
-        case "divide":
+        case "/":
             return divide(Number(num1), Number(num2));
     }   
 }
@@ -32,7 +32,7 @@ numberBtn.forEach((btn) => {
 
  
 document.addEventListener("keydown", (event) => {
-    if (Number(event.key)){
+    if (Number(event.key) || event.key == "0"){
         displayNum.push(event.key)
         display.textContent = displayNum.join("") 
         changeTextSize()  
@@ -42,19 +42,26 @@ document.addEventListener("keydown", (event) => {
 
 operatorBtn.forEach((btn) =>{
     btn.addEventListener("click", () =>{
-        if (operator == undefined && displayNum.length != 0){
-            num = displayNum.join("")
-            displayNum = [];
-        }
-        else if (operator != undefined && displayNum.length != 0) {
-            calculate()
-        }
-        operator = btn.id  
+        getOperator()
+        operator = btn.textContent  
     })
+})
+
+document.addEventListener("keydown", (event) => {
+    if (event.key == "+" || event.key == "-" || event.key == "/" || event.key == "*") {
+        getOperator()
+        operator = event.key
+    }
 })
 
 equals.addEventListener("click", () => {
    calculate()
+})
+
+document.addEventListener("keydown", (event) => {
+    if (event.key == "Enter"){
+        calculate()
+    }
 })
 
 clearBtn.addEventListener("click", () => {
@@ -62,17 +69,24 @@ clearBtn.addEventListener("click", () => {
 })
 
 decimalBtn.addEventListener("click", () => {
-    changeTextSize()
-    if (!displayNum.includes(".") && displayNum.length != 0){
-        displayNum.push(".")
-        display.textContent = displayNum.join("")
-    }
-    else if (displayNum.length == 0)
-    {
-        displayNum.push("0.");
-        display.textContent = displayNum.join("")
+   getDecimal()
+})
+
+document.addEventListener("keydown", (event) => {
+    if (event.key == "," || event.key == "."){
+        getDecimal()
     }
 })
+
+function getOperator() {
+    if (operator == undefined && displayNum.length != 0){
+        num = displayNum.join("")
+        displayNum = [];
+    }
+    else if (operator != undefined && displayNum.length != 0) {
+        calculate()
+    }
+}
 
 function calculate() {
     if (num != undefined && operator != undefined && displayNum.length != 0) {
@@ -98,6 +112,20 @@ function calculate() {
             operator = undefined 
         }
     }   
+}
+
+function getDecimal() {
+    changeTextSize()
+    if (!displayNum.includes(".") && displayNum.length != 0){
+        displayNum.push(".")
+        display.textContent = displayNum.join("")
+    }
+    else if (displayNum.length == 0)
+    {
+        displayNum.push("0");
+        displayNum.push(".")
+        display.textContent = displayNum.join("")
+    }
 }
 
 function changeTextSize() {
